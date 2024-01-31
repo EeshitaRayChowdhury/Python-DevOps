abe4271dcf2631ef818d5d900c300969e782b042
EESHITA RAY CHOWDHURY <47209556+EeshitaRayChowdhury@users.noreply.github.com>
2024-01-31 22:13:25 +0000

    Get string length example

---
+++ Day 2/string-len.py
@@ -0,0 +1,6 @@
+test = "I am the best"
+res = len(test)
+print("Length of string:",  + res) 
+
+# If you don't put , after " then this error will come up - print("Length of string:"  + res)
+# TypeError: can only concatenate str (not "int") to str 
