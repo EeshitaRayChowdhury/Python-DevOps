11f043fe1448851e14a751f4c85621697a7b44e7
EESHITA RAY CHOWDHURY <47209556+EeshitaRayChowdhury@users.noreply.github.com>
2024-01-31 22:25:21 +0000

    Split string examples

---
+++ Day 2/string-split.py
@@ -0,0 +1,22 @@
+#Example 1 - 
+#------------------------------------------
+
+
+text = "I am Eeshita"
+splitText = text.split()
+
+print("Words:", splitText)
+
+#O/P- Words: ['I', 'am', 'Eeshita']
+
+
+#Example 2
+#if you put a character with which you would like to divide in the split() bracket then the words are split from there for example 
+
+text = "I, am Eeshita"
+splitText = text.split(",")
+
+print("Words:", splitText)
+
+# O/P- Words: ['I', ' am Eeshita']
+ 
