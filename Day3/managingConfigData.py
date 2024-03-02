1716f69e965e9bd453adb9df111715a18373974b
EESHITA RAY CHOWDHURY <47209556+EeshitaRayChowdhury@users.noreply.github.com>
2024-03-02 09:25:12 +0000

    Create managingConfigData.py

---
+++ Day3/managingConfigData.py
@@ -0,0 +1,16 @@
+#Define the variables
+server_name = "my_server"
+port = 8080
+is_https_enabled = True
+max_connections = 3000
+
+# Print the configurations
+print(f"server_name : {server_name}")
+print(f"port : {port})
+print(f"HTTPS Enabled: {is_https_enabled}")
+print(f"Max Connections: {max_connections}")
+
+
+port = 443
+
+print(f"Updated port value: {port}")
